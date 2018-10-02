28e35b15d87350393c4c4ea29ebf26090351ae95
Don Mutua <print.idd@gmail.com>
2018-10-02 08:24:27 +0300

    creation of App.js and the test file with 2 checks

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    gifts: []
+  }
+  render() {
+    return (
+      <div>
+        <h3>Capital gift</h3>
+      </div>
+    )
+  }
+}
+
+export default App
